136f2e438af225959cda08bf951298280fce5cd3
MichaelCourter64 <michael.courter.64@gmail.com>
2018-12-10 15:33:49 -0800

    Added new project to try

---
+++ TestApp/TestApp/Program.cs
@@ -0,0 +1,52 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace TestApp
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            //SqlConnection sqlConnection = new SqlConnection("User ID=mAsterAcc;Password=fescdwax;Data Source=css475mariadbbasedgamelibrary.cuk3vrnaztl4.us-west-2.rds.amazonaws.com;" +
+                //"Integrated Security=false;Initial Catalog=Vapor;connection timeout=10");
+
+            SqlConnection sqlConnection = new SqlConnection("user id=mAsterAcc;" +
+                                       "password=fescdwax;server=css475mariadbbasedgamelibrary.cuk3vrnaztl4.us-west-2.rds.amazonaws.com;" +
+                                       "Trusted_Connection=yes;" +
+                                       "database=Vapor; " +
+                                       "connection timeout=10");
+
+            try
+            {
+                sqlConnection.Open();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+
+            try
+            {
+                sqlConnection.Close();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+
+            
+        }
+    }
+}
